0cc4aa4ac46069af43d87cc6795fd5dae66c00d3
njh8810 <njh8810@naver.com>
2022-07-28 15:57:31 +0900

    Add the TestWebApp Project

---
+++ WebApplication6/Models/Portfolio.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TestWebApp.Models
+{
+    /// <summary>
+    /// 모델 클래스 : Model, ViewModel, Dto, Object, Entity,....
+    /// </summary>
+    public class Portfolio
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Portfolio>(this);
+        }
+
+
+
+
+    }
+}
---
+++ WebApplication6/Services/PortfolioServiceJsonFile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using TestWebApp.Models;
+
+namespace TestWebApp.Services
+{
+    public class PortfolioServiceJsonFile
+    {
+        public IEnumerable<Portfolio> GetPortfolios()
+        {
+            var jsonFileName = @"C:\Users\jh.roh\source\repos\WebApplication6\WebApplication6\wwwroot\Portfolios\portfolios.json";
+
+            using(var jsonFileReader = File.OpenText(jsonFileName))
+            {
+                var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                
+                var portfolios = JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(), options);
+
+                return portfolios;
+            }
+        }
+
+
+
+
+    }
+}
