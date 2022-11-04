601d47a39a35943acd9ad32943dd492635911c33
njh8810 <njh8810@naver.com>
2022-11-04 22:27:01 +0900

    EA 아키텍처 용으로 솔루션 레이어 구성 추가(비즈니스, 데이터, 프레젠테이션)

---
+++ Note.BLL/UserBll.cs
@@ -0,0 +1,29 @@
+﻿using Note.IDAL;
+using Note.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Note.BLL
+{
+    public class UserBll
+    {
+        //private UserDal _userDal = new UserDal();
+
+        private IUserDal _userDal;
+
+        public UserBll(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public User GetUser(int userNo)
+        {
+            return _userDal.GetUser(userNo);
+        }
+
+        public List<User> GetUserList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
