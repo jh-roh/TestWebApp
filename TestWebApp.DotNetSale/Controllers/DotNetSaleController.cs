ba89edde8095d814fa04bb2046156052d1d6deed
njh8810 <njh8810@naver.com>
2022-11-24 20:35:13 +0900

    쇼핑몰 사이트 예제 cshtml 문서 작성 체크인

--- TestWebApp.DotNetSale/Controllers/DotNetSaleController.cs
+++ TestWebApp.DotNetSale/Controllers/DotNetSaleController.cs
@@ -19,9 +19,80 @@ namespace TestWebApp.DotNetSale.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult ProductAdd()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult ProductList(int categoryId = 0)
+        {
+            ViewBag.CategoryId = categoryId;
+
+            return View();
+        }
+
+        public IActionResult ProductPage()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ReviewList(int productId = 0)
+        {
+
+            ViewData["Product"] = productId;
+
+            return View();
+
+
+        }
+
+        public IActionResult AddToCart(
+            int productId = 0, int quantity = 0)
+        {
+            ViewBag.ProductId = productId;
+            ViewData["Quantity"] = quantity;
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult ShoppingCart()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ShoppingCart(
+            int productId = 0, int quantity = 0)
+        {
+            ViewBag.ProductId = productId;
+            ViewData["Quantity"] = quantity;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult SearchForm()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult SearchResult(string modelName)
+        {
+            ViewBag.ModelName = modelName;
+
+            return View();
+
+        }
     }
+
+
+
+
 }
