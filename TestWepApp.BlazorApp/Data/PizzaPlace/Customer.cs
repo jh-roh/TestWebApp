69f6bb1b3ba4a8c6c19021bd17f979061c38ce38
노준호 <njh8810@naver.com>
2024-04-21 00:20:45 +0900

    예제 Alert 콤포넌트 개발 및 체크인

--- TestWepApp.BlazorApp/Data/PizzaPlace/Customer.cs
+++ TestWepApp.BlazorApp/Data/PizzaPlace/Customer.cs
@@ -1,10 +1,18 @@
-﻿namespace TestWepApp.BlazorApp.Data.PizzaPlace
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestWepApp.BlazorApp.Data.PizzaPlace
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please provide a name")]
         public string Name { get; set; } = default!;
+
+        [Required(ErrorMessage = "Please provide a street with house number.")]
         public string Street { get; set; } = default!;
+
+        [Required(ErrorMessage = "Please provide a city")]
+
         public string City { get; set; } = default!;
     }
 }
