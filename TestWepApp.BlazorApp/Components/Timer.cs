34933705796970cf755a9c2e10d1076b44ae8b5f
노준호 <njh8810@naver.com>
2024-04-28 00:33:33 +0900

    Blazor 관련 예제소스 커밋

---
+++ TestWepApp.BlazorApp/Components/Timer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TestWepApp.BlazorApp.Components
+{
+    public class Timer : ComponentBase
+    {
+        [Parameter]
+        public double TimeInSeconds { get; set; }
+
+        [Parameter]
+        public Action Tick { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            var timer = new System.Threading.Timer(
+                callback: (_) => InvokeAsync(() => Tick?.Invoke()),
+                state: null,
+                dueTime: TimeSpan.FromSeconds(TimeInSeconds),
+                period: Timeout.InfiniteTimeSpan);
+              
+        }
+
+    }
+}
