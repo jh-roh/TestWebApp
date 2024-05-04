7bd43fe0580c2f4b99fb854b8f89ab084e826d14
노준호 <njh8810@naver.com>
2024-05-04 23:54:38 +0900

    Blazor 관련 예제소스 커밋(Binding 관련 CascadingParamter 예제)

---
+++ TestWepApp.BlazorApp/Components/CounterData.cs
@@ -0,0 +1,23 @@
+﻿namespace TestWepApp.BlazorApp.Components
+{
+    public class CounterData
+    {
+        private int count;
+
+        public int Count 
+        { 
+            get => this.count;
+            set
+            {
+                if(value != count)
+                {
+                    this.count = value;
+                    CountChanged?.Invoke(this.count);
+                }
+            }
+        
+        }
+
+        public Action<int>? CountChanged { get; set; }
+    }
+}
