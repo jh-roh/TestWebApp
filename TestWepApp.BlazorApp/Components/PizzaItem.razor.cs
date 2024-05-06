5acc1a2c30a5fa7a0f4fbf289c5ed690b2f237bc
노준호 <njh8810@naver.com>
2024-05-06 14:24:12 +0900

    LifeCycle 관련 내용 커밋 및 중첩컴포넌트 연습

---
+++ TestWepApp.BlazorApp/Components/PizzaItem.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using TestWepApp.BlazorApp.Data.PizzaPlace;
+
+namespace TestWepApp.BlazorApp.Components
+{
+    public partial class PizzaItem
+    {
+        [Parameter]
+        public Pizza Pizza { get; set; }
+
+        [Parameter]
+        public string ButtonTitle { get; set; }
+        [Parameter]
+        public string ButtonClass { get; set; }
+
+        [Parameter]
+        public EventCallback<Pizza> Selected { get; set; }
+
+        private string SpicinessImage(Spiciness spiciness) 
+            => $"images/{spiciness.ToString().ToLower()}.png";
+    }
+}
