43e6ac6105df6f7cab5d3384d4e7df0cfdebf4c3
노준호 <njh8810@naver.com>
2024-05-12 23:34:14 +0900

    Blaozr Advanced Template Componet 예제 커밋

---
+++ TestWepApp.BlazorApp/Components/Advanced/Grid.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TestWepApp.BlazorApp.Components.Advanced
+{
+    public partial class Grid<TItem>
+    {
+        [Parameter]
+        public RenderFragment Header { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment<TItem> Row { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment Footer { get; set; } = default!;
+
+        [Parameter]
+        public IReadOnlyList<TItem> Items { get; set; } = default!;
+
+
+    }
+}
---
+++ TestWepApp.BlazorApp/Components/Advanced/LIstView.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TestWepApp.BlazorApp.Components.Advanced
+{
+    public partial class  LIstView<TItem>
+    {
+        [Parameter]
+        public RenderFragment<TItem> ItemTemplate { get; set; } = default!;
+
+        [Parameter]
+        public IReadOnlyList<TItem> Items { get; set; } = default!;
+
+    }
+}
