991c814335b8dc76595b1be311ab8cbb4e406d5d
노준호 <njh8810@naver.com>
2024-05-20 23:45:43 +0900

    Paging 기능이 적용되어 있는 VirtualMeasurement 기능 예제 커밋

--- TestWepApp.Components.Library/Data/MeasurementsService.cs
+++ TestWepApp.Components.Library/Data/MeasurementsService.cs
@@ -39,5 +39,36 @@ namespace TestWepApp.Components.Library.Data
         }
 
 
+        public async ValueTask<(List<Measurement>, int)> GetMeasurementsPage
+            (int from, int count, CancellationToken cancellationToken)
+        {
+            const int maxMeasurements = 5000;
+
+            const int delay = 50;
+
+            await Task.Delay(delay, cancellationToken);
+
+
+            var result = new List<Measurement>();
+            var rnd = new Random();
+            
+            count = Math.Max(0, Math.Min(count, maxMeasurements - from));
+
+            for(int i = 0; i < count; i += 1)
+            {
+                result.Add(new Measurement()
+                {
+                    Guid = Guid.NewGuid(),
+                    Min = rnd.Next(0, 100),
+                    Avg = rnd.Next(100, 300),
+                    Max = rnd.Next(300, 400)
+                });
+            }
+
+            //return new ValueTask<(List<Measurement>, int)>((result, maxMeasurements));
+
+            return (result, maxMeasurements);
+        }
+
     }
 }
