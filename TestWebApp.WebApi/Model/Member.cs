1434c4b0ab97bdd9c93ec3b13fc8c38b1a859298
노준호 <njh8810@naver.com>
2024-08-10 14:03:06 +0900

    ASP.NET Core 미들웨어 관련된 내용 커밋

---
+++ TestWebApp.WebApi/Model/Member.cs
@@ -0,0 +1,8 @@
+﻿namespace TestWebApp.WebApi.Model
+{
+    public class Member
+    {
+        public string MemberName { get; set; } = "hong";
+        public string MemberGroup { get; set; } = "user";
+    }
+}
